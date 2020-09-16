c02189a9dd2d3b1e4b3a43695050c89cb991a7f2
Samuel Bernard Jeffersen <samuelbernard147@gmail.com>
2020-09-16 18:18:17 +0700

    :tada: Initial Commit

---
+++ bin/DartFundamental.dart
@@ -0,0 +1,25 @@
+import 'package:DartFundamental/DartFundamental.dart' as DartFundamental;
+
+// Single line comment
+/*
+  multi
+  line
+  comment
+*/
+/// documentation comment
+/** can add reference in comment
+    example : [main]
+ */
+
+
+void main(List<String> arguments) {
+  print('Hello world: ${DartFundamental.calculate()}!');
+
+  // variabel
+  var greetings = 'Hello Dart!';
+  var age;
+  age = 18;
+  print(greetings);
+  print( age);
+
+}
