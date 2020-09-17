3616df8c68d7a7c22ca64c4777780ae16df6d6fd
Samuel Bernard Jeffersen <samuelbernard147@gmail.com>
2020-09-17 10:54:47 +0700

    :pencil: Update String

--- bin/DartFundamental.dart
+++ bin/DartFundamental.dart
@@ -33,7 +33,7 @@ void main(List<String> arguments) {
   // Variabel
   String word = 'Kata '; // text
   int integer = 123; // integers
-  double decimal = 3.14; // decimal
+  double decimal =  .14; // decimal
   num number = -3484; // integers and decimal numbers
   bool boolean = true; // true or false
   List list = [1,2,3]; // list of value
@@ -43,6 +43,22 @@ void main(List<String> arguments) {
   dynamic = 'abc';
   dynamic = 123;
   dynamic = 3.14;
+
+  // Parsing
+  var eleven = int.parse('11');
+  var elevenPointTwo = double.parse('11.2');
+  var elevenAsString = 11.toString();
+  var piAsString = 3.14159.toStringAsFixed(2); // String piAsString = '3.14'
+
+  // String
+  String singleQuote = 'Ini adalah String';
+  String doubleQuote = "Ini juga String";
+  print('"What do you think of Dart?" he asked');
+  print("You're amazing");
+  print('"I think it\'s great!" I answered confidently'); // escape string
+  print("My name is $name, and i'm the fastest man alive..."); // String interpolation
+  print(r"He's just buy a $1,000.00 computer"); // 'r' Ignore interpolation
+  print('Hi \u2665'); // \uXXXX unicode
 
 
 }
